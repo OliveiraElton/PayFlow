e80467a7fc271cca91f23f907215fdf46aee1fd4
Elton Francisco de Oliveira <eltonf.oliveira22@gmail.com>
2021-06-21 23:26:23 -0300

    Desenvolvido telas SplashPage e LoginPage

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import 'modules/login/login_page.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: LoginPage(),
+    );
+  }
+}
