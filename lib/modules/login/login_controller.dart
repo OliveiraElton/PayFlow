57f1b83e40a416f1b169081206fd9d48004c09de
Elton Francisco de Oliveira <eltonf.oliveira22@gmail.com>
2021-06-26 11:37:29 -0300

    shared_preferences, lógica AuthController, Criando HomePage

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/models/user.model.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
 
 class LoginController {
@@ -12,7 +13,11 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
